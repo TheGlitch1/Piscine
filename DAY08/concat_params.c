933c2aa247f50be8d39ee8728a132dc5a34196bd
Faurest <faurest.lupine@epitech.eu>
2017-10-11 18:56:26 +0200

    fix

--- DAY08/concat_params.c
+++ DAY08/concat_params.c
@@ -1,14 +1,13 @@
 /*
- ** contact_params.c for CPool_Day08_2017 in /home/flupine/CPool_Day08_2017
+** concat_params.c for CPool_Day08_2017 in /home/flupine/CPool_Day08_2017
  ** 
  ** Made by Faurest Lupine
  ** Login   <faurest.lupine@epitech.eu>
  ** 
  ** Started on  mer. oct. 11 13:53:07 2017 Faurest Lupine
- ** Last update mer. oct. 11 14:39:32 2017 Faurest Lupine
+** Last update mer. oct. 11 17:04:20 2017 Faurest Lupine
  */
 
-#include <stdlib.h>
 #include "libmy.h"
 
 char	*concat_params(int argc, char **argv)
@@ -21,11 +20,11 @@ char	*concat_params(int argc, char **argv)
 	size = 0;
 	while (i < argc)
 	{
-		size = size + my_strlen(argv[1]);
+		size = size + my_strlen(argv[i]);
 		i++;
 	}
 	memoire = malloc(size * sizeof(char));
-	
+
 	if(memoire == NULL)
 		return("Erreur");
 
--- DAY08/my_strdup.c
+++ DAY08/my_strdup.c
@@ -5,10 +5,9 @@
 ** Login   <faurest.lupine@epitech.eu>
 ** 
 ** Started on  mer. oct. 11 10:01:22 2017 Faurest Lupine
-** Last update mer. oct. 11 11:46:32 2017 Faurest Lupine
+** Last update mer. oct. 11 18:54:59 2017 Faurest Lupine
 */
 
-#include <stdlib.h>
 #include "libmy.h"
 
 char	*my_strdup(char const *src)
@@ -16,7 +15,7 @@ char	*my_strdup(char const *src)
 	char		*memoire;
 	memoire =	malloc((my_strlen(src) + 1)  * sizeof(char));
 	if (memoire == NULL)
-		exit(0);
+		return ("error");
 	memoire = my_strcpy(memoire, src);
 	memoire[my_strlen(src) + 1] = '\0';
 	return(memoire);
